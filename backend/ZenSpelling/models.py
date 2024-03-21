46e94c5578ddb1f58a90efff69fb11dab26d4957
Zac Durham <43076672+PapaZ810@users.noreply.github.com>
2024-03-21 10:37:58 -0600

    auto student creation for users

--- backend/ZenSpelling/models.py
+++ backend/ZenSpelling/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Student(models.Model):
@@ -27,6 +29,15 @@ class Student(models.Model):
     def authenticate_user(username, password):
         user = authenticate(username=username, password=password)
         return user
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Student.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.student.save()
 
 
 class Course(models.Model):
