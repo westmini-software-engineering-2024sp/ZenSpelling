97651272a86717d556a470e43a56ff94281dde54
drizzy2004 <157053650+drizzy2004@users.noreply.github.com>
2024-03-22 18:44:27 -0600

    updated game setup page / music

--- backend/ZenSpelling/urls.py
+++ backend/ZenSpelling/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -13,4 +15,4 @@ urlpatterns = [
     path("ZenSpelling/<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("ZenSpelling/<int:question_id>/vote/", views.vote, name="vote"),
     path("tilepaths/", views.tile_paths, name="tilePaths"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
