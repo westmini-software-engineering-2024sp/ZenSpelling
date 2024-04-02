65b672c59b3c073711b7c58860d2786a280b75d6
Makenna Worley <makennaworley@gmail.com>
2024-04-02 10:52:53 -0600

    complete.html working

--- backend/ZenSpelling/static/ZenSpelling/js/complete.js
+++ backend/ZenSpelling/static/ZenSpelling/js/complete.js
@@ -1,18 +1,3 @@
-document.getElementById('clickable-image').addEventListener('click', function() {
-  let overlay = document.getElementById('overlay');
-  if (overlay.style.display === 'block') {
-    overlay.style.display = 'none';
-  } else {
-    overlay.style.display = 'block';
-  }
-  let clickableImage = document.getElementById('clickable-image');
-
-  // Check if the click event target is not the overlay or the clickable image
-  if (event.target !== overlay && event.target !== clickableImage) {
-    overlay.style.display = 'none'; // Hide the overlay
-  }
-});
-
 function getScore() {
     let correct = localStorage.getItem('correctAnswers');
     let total = localStorage.getItem('gameboardSize');
@@ -64,3 +49,17 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('streakDisplay').textContent = getStreakDisplay();
 });
 
+document.getElementById('clickable-image').addEventListener('click', function() {
+  let overlay = document.getElementById('overlay');
+  if (overlay.style.display === 'block') {
+    overlay.style.display = 'none';
+  } else {
+    overlay.style.display = 'block';
+  }
+  let clickableImage = document.getElementById('clickable-image');
+
+  // Check if the click event target is not the overlay or the clickable image
+  if (event.target !== overlay && event.target !== clickableImage) {
+    overlay.style.display = 'none'; // Hide the overlay
+  }
+});
