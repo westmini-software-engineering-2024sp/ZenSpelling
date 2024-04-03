af2403ab04b3469caeaf7df3c191e5a5f3d9093c
Makenna Worley <makennaworley@gmail.com>
2024-04-03 07:55:02 -0600

    tilestack is now long enough

--- backend/ZenSpelling/static/ZenSpelling/js/GameSetUp.js
+++ backend/ZenSpelling/static/ZenSpelling/js/GameSetUp.js
@@ -47,32 +47,40 @@ function generateQuestion(sidelength) {
     localStorage.setItem('correctAnswers', JSON.stringify(0));
 }
 
-// Fetch tilepath endpoints and load them in a stack.
-//How many tiles are loaded into the stack, how is that determined?
-function generateTileStack(gameboardSize) {
+/* Fetch tilepath endpoints and load them in a stack.
+** Contains algorithm to always fulfill the desired tileStack size.
+** This may not be needed if we make 25+ tiles.
+*/
+function generateTileStack() {
     let tileStack = [];
+
     fetch('/tilepaths/')
         .then(response => response.json())
         .then(data => {
+            let desiredCount = parseInt(localStorage.getItem('gameboardSize'));
             const fetchedTilePaths = data.tile_paths;
-            const repetitions = Math.ceil(gameboardSize / fetchedTilePaths.length);
-            const extendedTilePaths = Array.from({length: repetitions},
-                () => fetchedTilePaths).flat().slice(0, gameboardSize);
+            const repetitions = Math.ceil(desiredCount / fetchedTilePaths.length);
+            const extendedTilePaths = Array.from({ length: repetitions },
+                () => fetchedTilePaths).flat().slice(0, desiredCount);
             tileStack.push(...extendedTilePaths);
+
             shuffleTileStack(tileStack);
+
             localStorage.setItem('tileBank', JSON.stringify(tileStack));
         })
         .catch(error => console.error('Error fetching filepaths:', error));
 }
 
-
 // Fisher-Yates shuffle algorithm to randomize tiles.
 function shuffleTileStack(tileStack) {
+    // let tileStack = JSON.parse(localStorage.getItem('tileBank'));
+    console.log(tileStack.length);
     for (let i = tileStack.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [tileStack[i], tileStack[j]] = [tileStack[j], tileStack[i]];
     }
-    return tileStack;
+
+    localStorage.setItem('tileBank', JSON.stringify(tileStack));
 }
 
 function generateGameboard(size) {
